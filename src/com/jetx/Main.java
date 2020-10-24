027752c9a6b7e7f973ba69a4b03f097da75a43d1
Azenga Kevin <azenga.kevin7@gmail.com>
2020-10-24 18:09:37 +0300

    Discovered today, regardles of whether you launch a JFrame window in the main thread or use javax SwingUtils.invokeLater, the GUI runs on the event dispatcher thread, and the event dispatcher thread has a higher priority over the init thread commonly the main thread

--- src/com/jetx/Main.java
+++ src/com/jetx/Main.java
@@ -1,9 +1,30 @@
 package com.jetx;
 
+import javax.swing.SwingUtilities;
+
+import com.jetx.display.Display;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
+		SwingUtilities.invokeLater(() -> {
+			new Display("A Concurrency Test Frame", 600, 400);
+		});
+
+		Thread[] runningThreads = new Thread[5];
+
+		int result = Thread.enumerate(runningThreads);
+
+		System.out.println("Result: " + result);
+
+		for (Thread th : runningThreads) {
+			System.out.println("Thread Id: " + th.getId());
+			System.out.println("Thread Name: " + th.getName());
+			System.out.println("Thread Priority: " + th.getPriority());
+			System.out.println("Thread State: " + th.getState());
+			System.out.println();
+		}
 	}
 
 }
